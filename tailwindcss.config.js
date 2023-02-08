6a7f7ea2998c124105afb32aa70e7c9c168af731
JIMIN PARK <103014298+keepinblazing@users.noreply.github.com>
2023-02-08 22:24:19 +0900

    feat: add initial settings

---
+++ tailwindcss.config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    purge: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{ts,tsx,js,jsx}'],
+    darkMode: false,
+    theme: {},
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  };
