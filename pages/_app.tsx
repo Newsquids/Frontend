91c967bbe4aa3a10773c8cf7fbfbf76710142e37
JIMIN PARK <103014298+keepinblazing@users.noreply.github.com>
2023-03-14 21:50:06 +0900

    feat: add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,19 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import 'tailwindcss/tailwind.css';
+import Head from 'next/head';
 import { RecoilRoot } from 'recoil';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
+    <>
+    <Head>
+    <link rel='icon' href='/img/favicon.ico'/>
+    </Head>
     <RecoilRoot>
       <Component {...pageProps} />
     </RecoilRoot>
+    </>
   );
 }
 export default App
