0f6864bbce9096ed46081b98d62b46ff2ccbd4a1
JIMIN PARK <103014298+keepinblazing@users.noreply.github.com>
2023-05-12 21:06:40 +0900

    fix: remove bookmark

--- components/news/NewsHeadline.tsx
+++ components/news/NewsHeadline.tsx
@@ -49,13 +49,6 @@ const NewsHeadline: FC<NewsHeadlineProps> = ({
             <div className='relative w-[90%] h-full' id={newsHeadline}>
               <div className='flex flex-col justify-center items-start gap-3 p-2 relative'>
                   <em>{isTodayNews ? `${newsChannel?.toUpperCase()} /  HEADLINE` : 'HEADLINE'}</em>
-                  {isBookmarked ? (
-                    <button className='text-3xl z-30 absolute right-1'>
-                      ★
-                    </button>
-                  ) : (
-                    <button className='text-3xl z-30 absolute right-1' onClick={() => handleClickBookmark()}>☆</button>
-                  )}
                 <h1 className='md:text-md lg:text-lg text-left'>{newsHeadline}</h1>
               </div>
               <div className='absolute bottom-1 right-2'>
